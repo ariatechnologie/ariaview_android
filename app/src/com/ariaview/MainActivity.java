fc1e8473910744f541ab5c8f8a64b13fb59e1b05
odasilva <odasilva@iut.univ-paris8.fr>
2015-03-12 11:47:11 +0100

    Correction fichier test xml

--- app/src/com/ariaview/MainActivity.java
+++ app/src/com/ariaview/MainActivity.java
@@ -16,22 +16,32 @@ import org.xml.sax.SAXException;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-	private String login1XML;
+	private String UrlTest = "https://raw.githubusercontent.com/ariatechnologie/ariaview_android/master/testFile/";
+	private String login1XML = "login.xml";
+	private String login2XML = "login2.xml";
+	private String datesXML = "dates.xml";
+	
+	
 	private Document document;
 	private DocumentBuilderFactory fabrique;
 	private DocumentBuilder constructeur;
 	private NodeList sites;
+	private File ariaDirectory;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		ariaDirectory = new File("/sdcard/AriaView/");
+		ariaDirectory.mkdirs();
+		
 		fabrique = DocumentBuilderFactory.newInstance();
 		
 		Button btn = (Button) findViewById(R.id.loginButton);
@@ -43,25 +53,27 @@ public class MainActivity extends Activity {
 
 		//SEND LOGIN 1
 		
+		final DownloadTask downloadTask = new DownloadTask(MainActivity.this);
+		downloadTask.execute(UrlTest+login1XML);
 		
-		//login1XML = "bin/classes/login.xml";
-		InputStream is = getResources().getAssets().open("testFile/login.xml");
-		login1XML = getResources().getAssets().toString();
+
+		//String fileXML = Environment.getExternalStorageDirectory()+"/AriaView/"+login1XML;
 		
-		System.out.println(login1XML);
+		File fileXML = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath()+"/AriaView/", login1XML);
+		
 		
 		try {
 			constructeur = fabrique.newDocumentBuilder();
-			document = constructeur.parse(login1XML);
+			document = constructeur.parse(fileXML);
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		}
 		
-		sites = document.getElementsByTagName("site");
-		
-			
-			//		intent = new Intent(this, FavoriteActivity.class);
-//		startActivity(intent);
+		sites = document.getElementsByTagName("site");		
+	
+		//		intent = new Intent(this, FavoriteActivity.class);
+		//		startActivity(intent);
 	}
 	
 	
